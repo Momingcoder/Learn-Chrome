5b7a6518032a94002d50edb0ce3c6de58800cf8c
MomingCoder <a398445075@gmail.com>
2016-05-09 13:02:49 +0800

    stupid mistake

--- my_clock/js/my_clock.js
+++ my_clock/js/my_clock.js
@@ -1,14 +1,13 @@
-function my_clock(el) {
+function my_clock() {
     var today = new Date();
     var h = today.getHours();
-    var m = today.getHours();
+    var m = today.getMinutes();
     var s = today.getSeconds();
     h = h >= 10 ? h : ('0' + h);
     m = m >= 10 ? m : ('0' + m);
     s = s >= 10 ? s : ('0' + s);
-    el.innerHTML = h + ":" + m + ":" + s;
-    setTimeout(function() {my_clock(el)}, 1000);
+    document.getElementById('clock_div').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(my_clock, 500);
 }
 
-var clock_div = document.getElementById('clock_div');
-my_clock(clock_div);
+my_clock();
