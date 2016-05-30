eb38660d7fb5adb284ec90988fc2b30fdd5f7613
MomingCoder <a398445075@gmail.com>
2016-05-30 16:28:16 +0800

    beautify weather forecast

--- weather-forecast/js/weather.js
+++ weather-forecast/js/weather.js
@@ -12,17 +12,31 @@ function httpRequest(url, callback){
 function showWeather(result) {
     result = JSON.parse(result);
     var list = result.list;
-    var table = '<table><tr><th>Date</th><th>Weather</th><th>Low T</th><th>High T</th></tr>';
+    var index = 1;
+    var table = '<p class = "top">City: ' + localStorage.city + '</p>';
+    table += '<table><tr><th>Date</th><th>Weather</th><th>Low T</th><th>High T</th></tr>';
     for(var i in list) {
         var d = new Date(list[i].dt * 1000);
-        table += '<tr>';
-        table += '<td>' + d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + '</td>';
-        table += '<td>' + list[i].weather[0].description + '</td>';
-        table += '<td>' + Math.round(list[i].temp.min - 273.15) + ' °C</td>';
-        table += '<td>' + Math.round(list[i].temp.max - 273.15) + ' °C</td>';
-        table += '</tr>';
+        if (i % 2 == 0) {
+            table += '<tr class = "alt">';
+            table += '<td>' + d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + '</td>';
+            table += '<td>' + list[i].weather[0].description + '</td>';
+            table += '<td>' + Math.round(list[i].temp.min - 273.15) + ' °C</td>';
+            table += '<td>' + Math.round(list[i].temp.max - 273.15) + ' °C</td>';
+            table += '</tr>';
+        }
+        else {
+            table += '<tr>';
+            table += '<td>' + d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + '</td>';
+            table += '<td>' + list[i].weather[0].description + '</td>';
+            table += '<td>' + Math.round(list[i].temp.min - 273.15) + ' °C</td>';
+            table += '<td>' + Math.round(list[i].temp.max - 273.15) + ' °C</td>';
+            table += '</tr>';
+        }
+        index += 1;
     }
     table += '</table>';
+    table += '<p class = "buttom">Source: http://openweathermap.org </p>';
     document.getElementById('Weather').innerHTML = table;
 }
 
